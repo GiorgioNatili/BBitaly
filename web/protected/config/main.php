5f010c48f571c5ac8e76353b54c911ea40268b33
ArkeologeN <hamzawaqas.programmer@gmail.com>
2013-09-18 09:00:35 -0700

    env. variable + environment loader added

--- web/protected/config/main.php
+++ web/protected/config/main.php
@@ -10,7 +10,7 @@ return array(
 	'name'=>'BBitaly',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload' => array('log'),
 
 	// autoloading model and component classes
 	'import'=>array(
@@ -35,6 +35,28 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
+                'facebook'=>array(
+                    'class' => 'ext.yii-facebook-opengraph.SFacebook',
+                    'appId'=>'1394038854160155', // needed for JS SDK, Social Plugins and PHP SDK
+                    'secret'=>'67504784061e1d32e6def7dd974a6c31', // needed for the PHP SDK
+                    //'fileUpload'=>false, // needed to support API POST requests which send files
+                    //'trustForwarded'=>false, // trust HTTP_X_FORWARDED_* headers ?
+                    //'locale'=>'en_US', // override locale setting (defaults to en_US)
+                    'jsSdk'=>true, // don't include JS SDK
+                    'async'=>true, // load JS SDK asynchronously
+                    //'jsCallback'=>false, // declare if you are going to be inserting any JS callbacks to the async JS SDK loader
+                    //'status'=>true, // JS SDK - check login status
+                    //'cookie'=>true, // JS SDK - enable cookies to allow the server to access the session
+                    //'oauth'=>true,  // JS SDK - enable OAuth 2.0
+                    //'xfbml'=>true,  // JS SDK - parse XFBML / html5 Social Plugins
+                    //'frictionlessRequests'=>true, // JS SDK - enable frictionless requests for request dialogs
+                    //'html5'=>true,  // use html5 Social Plugins instead of XFBML
+                    //'ogTags'=>array(  // set default OG tags
+                        //'title'=>'MY_WEBSITE_NAME',
+                        //'description'=>'MY_WEBSITE_DESCRIPTION',
+                        //'image'=>'URL_TO_WEBSITE_LOGO',
+                    //),
+                  ),
 		// uncomment the following to enable URLs in path-format
 		/*
 		'urlManager'=>array(
