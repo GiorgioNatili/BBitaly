3bc58b68d118747af17ebd7b73d2a8fa55fe7edc
ArkeologeN <hamzawaqas.programmer@gmail.com>
2013-10-03 10:39:22 -0700

    assets modified 0310

--- web/themes/bbitalyv1/assets/js/bootstrap.custom.js
+++ web/themes/bbitalyv1/assets/js/bootstrap.custom.js
@@ -8,28 +8,6 @@ $(function () {
     $('#account_tabbable').bootstrapWizard({'tabClass': 'account-tabs'});
 	
 	$('.jcarousel-horizontal').jcarousel({scroll:1});
-        $('#add-location').click(function() {
-           // Append Location
-           var sno = $('.itin-loca-count').size() +1;
-           var html = '<li>'
-                     + '<div class="itin-loca-count count">'+sno+'</div>'
-                        + '<div class="outcome">'
-                            + '<div class="name"><input type="text" class="search-query" name="location['+sno+'][name]" placeholder="Name the itinerary location.." /></div>'
-                            + '<div class="date"><i class="icon-flag"></i> 10/01/2013</div>'
-                            + '<div class="date"><i class="icon-flag"></i> 10/01/2013</div>'
-                            + '<div class="day"><i class="icon-flag"></i> 10</div>'
-                            + '<div class="delete"><a href="#"><i class="icon-flag"></i> D</a></div>'
-                        + '</div>'
-                    + '</li>';
-           $('#itin-loc-list-end').before(
-               html
-            );
-        });
-    
+	
+	$('.datepicker').datepicker()
 });
-
-function choosePolicy(i) {
-    var $ins = $(i);
-    $('#fPolicy').val($ins.val());
-}
-
