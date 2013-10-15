db716ea5dd2686f7aaf94cacd73ef6bcb8ae77a4
ArkeologeN <hamzawaqas.programmer@gmail.com>
2013-10-15 11:44:44 -0700

    Favorites listing updated with delete function

--- web/themes/bbitalyv1/views/favorites/_view.php
+++ web/themes/bbitalyv1/views/favorites/_view.php
@@ -3,13 +3,12 @@
 /* @var $data Favorites */
 ?>
 
-<?php //echo "<pre>"; print_r($data->property); exit; ?>
 
-<li class="item">
+<li class="item" id="itin-row-<?php echo $index ?>">
     <div class="row">
         <div class="span3">
             <div class="img">
-                <img src="<?php echo Yii::app()->theme->baseUrl ?>/assets/img/bb_list-img-1.jpg" alt="" />
+                <img src="<?php echo $this->getAssetsUrl() ?>img/bb_list-img-1.jpg" alt="" />
             </div>
         </div>
         <div class="span7">
@@ -18,7 +17,7 @@
                 <p class="lead">Nome della struttura ricettiva, consectetur adipis</p>
                 <p>Descrizione breve della struttura lorem ipsum dolor sit amet, consectetur adipis</p>
                 <p class="location">
-                    <i class="icon-arrow-down"></i> 
+                    <i class="icon-location"></i> 
                     <?php echo $data->property->address.', '. $data->property->zip_code ?> / <strong><?php echo $data->property->city ?></strong>
                 </p>
             </div>
@@ -32,8 +31,8 @@
                     </p>
                 </div>
                 <div class="favorite-tag text-left">
-                    <span class="count text-center"><?php echo $data->property->favorites ?></span>
-                    <a href="#">preferito</a>
+                    <span class="count text-center"><?php echo count($data->property->favorites) ?></span>
+                    <a data-pid="<?php echo $data->property->id ?>" onclick="removeFromFavorites(this, function() { $('#itin-row-<?php echo $index ?>').remove(); });">preferito</a>
                 </div>
             </div>
         </div>
